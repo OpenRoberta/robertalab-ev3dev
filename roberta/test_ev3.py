b0a2a4f83611b707ae702e418d08ac4fdacfeb6d
Stefan Sauer <ensonic@users.sf.net>
2017-12-11 21:08:01 +0100

    tests: add another test for a basic config

--- roberta/test_ev3.py
+++ roberta/test_ev3.py
@@ -1,9 +1,22 @@
-import logging
 import unittest
 
 from .ev3 import Hal
 
 
 class TestHal(unittest.TestCase):
-    def test__init__(self):
+    def test__init__no_cfg(self):
         hal = Hal(None)
+        self.assertNotEqual(0, hal.font_w)
+        self.assertNotEqual(0, hal.font_h)
+
+    def test__init__simple_cfg(self):
+        brickConfiguration = {
+            'wheel-diameter': 5.6,
+            'track-width': 18.0,
+            'actors': {
+            },
+            'sensors': {
+            },
+        }
+        hal = Hal(brickConfiguration)
+        self.assertIn('wheel-diameter', hal.cfg)
